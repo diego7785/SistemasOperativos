8b6e2e5a724429162460fad44c9dcd15edd661a6
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-06-16 14:12:55 +0000

    Tarea 1

--- tarea_01_ostep/io.c
+++ tarea_01_ostep/io.c
@@ -7,14 +7,16 @@
 #include <string.h> 
 #include <stdio.h>
 
-//FILE *file;
-//char caracter;
+FILE *file;
+char caracter;
+//int tamano;
 
 int exists(const char *fname) {
-    FILE *file;
+   // FILE *file;
     if ((file = fopen(fname, "r")))
     {
-      fclose(file);
+     // tamano = sizeof(file);
+     // fclose(file);
       return 1;
     }
     return 0;
@@ -24,24 +26,27 @@ int main(int argc, char *argv[]) {
    char *entrada = argv[1];
    int existencia = exists(entrada); //0 = NO existe, 1 SI existe
    if(existencia == 1){
-  // while((caracter = fgetc(file)) != EOF){
-	//printf("%c",caracter);
-     char buf[100];
-     size_t nbytes;
-     int fd = open(entrada, O_RDONLY);
+   fseek(file, 0L, SEEK_END);
+   printf("%d bytes", ftell(file));
+   while((caracter = fgetc(file)) != EOF){
+	printf("%c",caracter);
+    // char buf[100];
+    // size_t nbytes;
+    // int fd = open(entrada, O_RDONLY);
     // printf("%d",fd);
-     ssize_t archivo = read(fd, buf, 10);
-     close(fd);
-     if(archivo == 0){
-	printf("El archivo esta vacio \n");
-     }else{
-	printf("%s \n", buf);
-     }
-  // }
+    // nbytes = tamano;
+    // ssize_t archivo = read(fd, buf, nbytes);
+    // close(fd);
+    // if(archivo == 0){
+	//printf("El archivo esta vacio \n");
+    // }else{
+	//printf("%s \n", buf);
+     //}
+   }
    }else{
    printf("El archivo no existe \n");
    }
-   
+   fclose(file);
    return 0;
 }
 
