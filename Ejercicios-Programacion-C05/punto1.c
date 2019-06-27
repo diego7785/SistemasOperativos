b0f2a8b7e70eabc50ca2d93f7f62ce4e0f33d3bd
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-06-27 04:03:42 +0000

    Ejercicios capítulo 5

--- Ejercicios-Programacion-C05/punto1.c
+++ Ejercicios-Programacion-C05/punto1.c
@@ -4,11 +4,22 @@
 
 int main(int argc, char** argv){
 	int x = 100;
-	fork();
-	x=x+2;
-	printf("%d \n",x);
+	int pid = fork();
+	if(pid==0){
+	  printf("Hijo: \n");
+	  printf("X vale: %d \n",x);
+	  x=500;
+	  printf("Cambio hecho por el hijo: %d \n", x);
+	} else {
+	  printf("Padre: \n");
+	  printf("X vale: %d \n",x);
+	  x=50;
+	  printf("Cambio hecho por el padre: %d \n",x);
+	}
 	return 0;
 }
 
-//El valor de x que ambos ven es: 100
-//Si ambos editan el valor de x, x valdrá lo mismo para ambos procesos (102), perdiéndose así una asignación
+//El valor de x que ve el hijo es 100
+//Si ambos editan el valor de x, en cada caso x valdrá justamente el valor editado
+//Ya que los procesos, aunque uno es copia del otro, estos no interfieren en su ejecución ni en sus variables porque tienen espacios 
+//diferentes en memoria
