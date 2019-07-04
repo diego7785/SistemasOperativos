c03eeb8ab564b6861ba23a565a2dfc0d92952853
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-03 20:43:44 -0500

    Ejercicios capítulo 5

--- Ejercicios-Programacion-C05/punto2.c
+++ Ejercicios-Programacion-C05/punto2.c
@@ -10,11 +10,13 @@ int main(int argc, char *argv[]){
   int pid = fork();
   if(pid==0){
     printf("Proceso hijo: \n");
+    write(file, "Proceso hijo escribiendo \n", strlen("Proceso hijo escribiendo \n"));
   }else{
     printf("Proceso padre: \n");
+    write(file, "Proceso padre escribiendo \n", strlen("Proceso padre escribiendo \n"));
   }
   FILE *fil;
-  fil=fopen("hola", "r+");
+  fil=fopen("hola", "r");
   int c;
   while((c = fgetc(fil)) != EOF){
     printf("%c",c);
