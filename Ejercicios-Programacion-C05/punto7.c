9fbf1ea61867ee4de9d6a81ff7012e4d108b646a
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-08 02:56:42 -0500

    Ejercicios capítulo 5

--- Ejercicios-Programacion-C05/punto7.c
+++ Ejercicios-Programacion-C05/punto7.c
@@ -5,20 +5,15 @@
 int main(int argc, char *argv[])
 {
 	int rc = fork();
-	if (rc < 0)
+	if (rc == 0)
 	{
-		fprintf(stderr,"fork failed\n");
-		exit(1);
-	}
-	else if (rc == 0)
-	{
-		printf("I am child\n");
+		printf("Proceso hijo \n");
 		close(STDOUT_FILENO);
-		printf("I have closed standard output\n");
+		printf("Se cerró la salida estándar \n");
 	}
 	else 
 	{
-		printf("I am parent\n");
+		printf("Proceso padre \n");
 	}
 	return 0;
 }
