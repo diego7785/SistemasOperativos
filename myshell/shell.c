e8f3a7189cd975aca5c05dd948d5fa26d1d21da4
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-08 19:30:26 +0000

    Shell

--- myshell/shell.c
+++ myshell/shell.c
@@ -6,8 +6,7 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-//	printf("%d", strcmp(argv[1], "cd"));
-  if(argc != 2){
+  if(argc < 1){
       printf("Missing arguments \n");
       exit(1);
     }else{
@@ -15,20 +14,22 @@ int main(int argc, char** argv){
     DIR *dirp;
     struct dirent *direntp;
 
-    if(argc != 2){
-      printf("Uso: %s directorio \n", argv[0]);
+    if(argc < 2){
+     // printf("Uso: %s directorio \n", argv[0]);
+      printf("Missing arguments in cd \n");
       exit(1);
     }
 
-    dirp=opendir(argv[1]);
+    dirp=opendir(argv[2]);
+   // printf("[%s]", dirp);
     if(dirp==NULL){
       printf("Error: No se puede abrir el directorio \n");
       exit(2);
     }
 
-    printf("i-nodo\toffset\t\tlong\nombre\n");
+   
     while((direntp=readdir(dirp)) != NULL){
-      printf("HOLA");
+      printf("[%s]\n", direntp->d_name);
     }
 
     closedir(dirp);
