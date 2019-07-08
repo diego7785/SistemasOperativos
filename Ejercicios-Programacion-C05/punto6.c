873f87b2caac14a4eb361d5ba95dca419d5a95b6
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-08 02:50:06 -0500

    Ejercicios capítulo 5

---
+++ Ejercicios-Programacion-C05/punto6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char * argv[])
+{
+	int rc = fork();
+	if (rc == 0)
+	{
+		printf("Proceso hijo en ejecución \n");
+	}
+	else 
+	{
+		int w=waitpid(rc,NULL,WUNTRACED);
+		printf("Proceso padre en ejecución \n");
+    printf("id wait: %d", w);
+	}
+	return 0;
+}
