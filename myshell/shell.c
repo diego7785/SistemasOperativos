6970ff5b85eb1bfb966a3f990f6db7ee4e605cda
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-09 01:26:18 +0000

    Shell

--- myshell/shell.c
+++ myshell/shell.c
@@ -6,44 +6,44 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-  if(argc < 1){
-      printf("Missing arguments \n");
-      exit(1);
-    }else{
-  if(strcmp(argv[1], "cd")==0){
-    DIR *dirp;
-    struct dirent *direntp;
+	char comandos[1000];
+	while(1){
+		printf("%s shell>", getenv("PWD"));
+  		scanf("%s", comandos);
+  		
+    		DIR *dirp;
+    		struct dirent *direntp;
 
-    if(argc < 2){
-     // printf("Uso: %s directorio \n", argv[0]);
-      printf("Missing arguments in cd \n");
-      exit(1);
-    }
+    		if(argc < 2){
+     		// printf("Uso: %s directorio \n", argv[0]);
+     		 printf("Missing arguments in cd \n");
+     		 exit(1);
+    		}
 
-    dirp=opendir(argv[2]);
-   // printf("[%s]", dirp);
-    if(dirp==NULL){
-      printf("Error: No se puede abrir el directorio \n");
-      exit(2);
-    }
+    		dirp=opendir(argv[2]);
+   		// printf("[%s]", dirp);
+    		if(dirp==NULL){
+      			printf("Error: No se puede abrir el directorio \n");
+      		exit(2);
+    		}
 
    
-    while((direntp=readdir(dirp)) != NULL){
-      printf("[%s]\n", direntp->d_name);
-    }
+    		while((direntp=readdir(dirp)) != NULL){
+      			printf("[%s]\n", direntp->d_name);
+    		}
 
-    closedir(dirp);
+    		closedir(dirp);
     
-  }else if(strcmp(argv[1], "pwd")==0){
-    char current_pos[100];
-    getcwd(current_pos, sizeof(current_pos));
+  		 
+    		char current_pos[100];
+    		getcwd(current_pos, sizeof(current_pos));
     
-	printf("%s \n", current_pos);
+		printf("%s \n", current_pos);
     
-  printf("\n");
-  }else{
-    printf("Missing arguments");
-  }
-}
+  		printf("\n");
+  
+   		// printf("Missing arguments");
+  
+	}
   return 0;
 }
