a3aa312195cc95f8605fc9d9b8cb1e6d406d10bf
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-09 16:58:15 +0000

    Helper

--- myshell/helper.h
+++ myshell/helper.h
@@ -1,13 +1,17 @@
 int onlyComands(int start, int end, char comando[], char tipo[]){
-        int size=sizeof(&comando)/sizeof(comando[0]); 
-        char comand[size];
+        int size=end-start;
+        char* comand;
+        comand = (char*)malloc(size*sizeof(char));
         for(int i=start, j=0; i<end; i++,j++){
                 comand[j]=comando[i];
         }
+
         if(strcmp(comand,tipo)==0){
                 return 1;
         }else{
                 return 0;
         }
+        free(comand);
 }
+
 
