c65d0914084467173f6a1341cbd18a262252db33
Diego Bonilla <diego.viafara@correounivalle.edu.co>
2019-07-09 16:59:49 +0000

    Archivo de prueba

--- myshell/helper.c
+++ myshell/helper.c
@@ -4,31 +4,28 @@
 
 int onlyComands(int start, int end, char comando[], char tipo[]){
 	int size=end-start;
-	char comand[size];
+	char* comand;
+	comand = (char*)malloc(size*sizeof(char));
         for(int i=start, j=0; i<end; i++,j++){
                 comand[j]=comando[i];
         }
 	printf("%s \n", comando);
 	printf("%s \n", comand);
-	printf("%s \n", tipo);
 
 	if(strcmp(comand,tipo)==0){
 		return 1;
 	}else{
 		return 0;
 	}
+	free(comand);
 }
 
 int main(){
 	char comando[]="cd /home";
-	char test[sizeof("cd")];
-	strcpy(test,"cd");
-	int size=strlen(test);
-	char test2[size];
-
-	printf("%s \n", test2);
-	//int n=onlyComands(0,2,comando,test2);
-	//printf("%d \n", n);
+	char test[]="cd";
+	//printf("%s \n", test2);
+	int n=onlyComands(0,2,comando,"cd");
+	printf("%d \n", n);
 return 0;
 }
 
