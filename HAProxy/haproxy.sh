231b838de897a5108b34e3d223ded3967143dfdd
diego7785 <diego.viafara@correounivalle.edu.co>
2019-09-17 17:10:18 -0500

    Tarea final

--- HAProxy/haproxy.sh
+++ HAProxy/haproxy.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
 apt-get update
 apt-get install -y haproxy
+cd /etc/default
+chmod 666 haproxy
+sudo echo "ENABLED=1" >> /etc/default/haproxy
+sudo rm /etc/haproxy/haproxy.cfg
+cd /vagrant
+sudo cp haproxy.cfg /etc/haproxy
+sudo service haproxy start
+sudo service haproxy restart
